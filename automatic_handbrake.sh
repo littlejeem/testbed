a2c4fd187ac8fb87ff6f8feff2421c758ed81eb8
James Livingstone <james@jamess-mbp.home>
2020-07-09 22:04:00 +0100

    alteration to parsing data based on testing

--- automatic_handbrake.sh
+++ automatic_handbrake.sh
@@ -68,8 +68,11 @@ HandBrakeCLI --json -i /home/jlivin25/Rips/blurays/THE_DARK_KNIGHT -t $auto_find
 sed -n '/JSON Title Set: {/,$w main_feature_scan_trimmed.json' main_feature_scan.json
 #now we need to delete the top line left as "JSON Title Set: {"
 sed -i '1d' main_feature_scan_trimmed.json
-#we now  need to insert a spare {' at the start of the file
+#we now  need to insert a spare '{' & a '[' at the start of the file
 sed -i '1s/^/{\n/' main_feature_scan_trimmed.json
+sed -i '1s/^/[\n/' main_feature_scan_trimmed.json
+#and now we need to add ']' to the end of the file
+echo "]" >> main_feature_scan_trimmed.json
 #now trim out the error line where '  HandBrake has exited.' is in the middle of the .json data
 sed -i '/  HandBrake has exited./d' main_feature_scan_trimmed.json
 #at this point the data is ready for 'parsing'
