3aadbcc893b383e319382a146715b8f80ab1fa16
James Livingstone <james@jamess-mbp.home>
2020-07-14 18:54:07 +0100

    picked up drive nomenclature error

--- automatic_handbrake.sh
+++ automatic_handbrake.sh
@@ -8,7 +8,7 @@ source_drive="disc:0"
 dev_drive="/dev/sr0"
 working_dir="/home/jlivin25/Rips"
 rip_dest="blurays"
-bluray_name=$(blkid -o value -s LABEL "$source_drive")
+bluray_name=$(blkid -o value -s LABEL "$dev_drive")
 bluray_name=${bluray_name// /_}
 makemkvcon backup "$source_drive" "$working_dir"/"$rip_dest"/"$bluray_name"
 #
