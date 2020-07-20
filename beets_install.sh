8d399449948e6e9e9f231573c58d96581cfc5710
James Livingstone <james@jamess-mbp.home>
2020-07-20 19:54:27 +0100

    initial commits based on testing

--- beets_install.sh
+++ beets_install.sh
@@ -1,1 +1,41 @@
-/home/jlivin25/Music/Library/alacimports
+#!/usr/bin/env bash
+#
+assigned_user="jlivin25"
+#
+#+------------------------------------+
+#+---"Test for root running script"---+
+#+------------------------------------+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+#
+#
+#+------------------------+
+#+---"Install main app"---+
+#+------------------------+
+apt update
+apt install python-dev python-pip -y
+#
+#
+#+----------------------------------------+
+#+---"Install dependancies for plugins"---+
+#+----------------------------------------+
+# chroma
+sudo -u $assigned_user pip install pyacoustid
+sudo -u $assigned_user pip install gmusicapi
+apt install -y libchromaprint-tools
+#
+#
+#+--------------------------------+
+#+---"Set up default locations"---+
+#+--------------------------------+
+# conversion destinations
+mkdir -p $HOME/Music/Library/alacimports
+mkdir -p $HOME/Music/Library/flacimports/
+mkdir -p $HOME/Music/Library/PlayUploads/
+# library file sources
+mkdir -p $HOME/.config/beets/alac/
+mkdir -p $HOME/.config/beets/flac/
+mkdir -p $HOME/.config/beets/upload/
