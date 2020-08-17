7e5832e613a144d1d1d5c8c2e7ef2dfe777b06f7
James Livingstone <james@jamess-mbp.home>
2020-08-17 19:40:40 +0100

    expanded for testing

--- geopts_play.sh
+++ geopts_play.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 #
 #
-while getopts t:q: flag
+while getopts t:q:s:c: flag
 do
     case "${flag}" in
         t) title_override=${OPTARG};;
         q) quality_override=${OPTARG};;
+        s) source_clean_override=${OPTARG};;
+        c) temp_clean_override=${OPTARG};;
     esac
 done
-echo "title: $title_override";
-echo "quality: $quality_override";
+echo "title overriden now: $title_override";
+echo "quality overriden, now: $quality_override";
+echo "cleaning source overriden?: $source_clean_override";
+echo "cleaning temp overriden?: $temp_clean_override";
