66fd0387b484151f66dc59f94f67ea0be8319084
James Livingstone <james@jamess-mbp.home>
2020-08-17 19:57:54 +0100

    adjusted to include +ve & -ve affirmations

--- geopts_play.sh
+++ geopts_play.sh
@@ -17,20 +17,24 @@ echo "cleaning temp overriden?: $temp_clean_override";
 
 if [[ $title_override == "" ]]; then
   echo "no title override applied"
+elif [[ $title_override == "y" ]]; then
+  echo -e "title override selected\chosen title is $title_override"
 fi
 
 if [[ $quality_override == "" ]]; then
   echo "no quality override applied"
+elif [[ $quality_override == "y" ]]; then
+  echo -e "quality override selected\chosen title is $quality_override"
 fi
 
 if [[ $source_clean_override == "" ]]; then
   echo "no source clean override selected"
 elif [[ $source_clean_override == "y" ]]; then
-  echo -e "source clean override applied" \ "not deleting source files"
+  echo -e "source clean override applied\not deleting source files"
 fi
 
 if [[ $tmep_clean_override == "" ]]; then
   echo "no temp files clean override selected"
 elif [[ $temp_clean_override == "y" ]]; then
-  echo -e "temp clean override applied" \ "keeping temp files for debugging"
+  echo -e "temp clean override applied\keeping temp files for debugging"
 fi
