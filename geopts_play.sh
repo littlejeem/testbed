432b8d4cd61dbe80e2197604fb4313c4645d38e6
James Livingstone <james@jamess-mbp.home>
2020-08-17 20:14:20 +0100

    removed echo newlines for now

--- geopts_play.sh
+++ geopts_play.sh
@@ -19,7 +19,7 @@ if [[ $title_override == "" ]]; then
   echo "no title override applied"
 #now test to make sure a number, see @Joseph Shih answer here https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
 elif echo "$title_override" | grep -qE '^[0-9]+$'; then
-  echo -e "title override selected\chosen title is $title_override"
+  echo -e "title override selected, chosen title is $title_override"
   else
     echo "Error: -t is not a number."
     exit 2
@@ -28,17 +28,17 @@ fi
 if [[ $quality_override == "" ]]; then
   echo "no quality override applied"
 elif [[ $quality_override == "y" ]]; then
-  echo -e "quality override selected\chosen title is $quality_override"
+  echo -e "quality override selected, chosen title is $quality_override"
 fi
 
 if [[ $source_clean_override == "" ]]; then
   echo "no source clean override selected"
 elif [[ $source_clean_override == "y" ]]; then
-  echo -e "source clean override applied\not deleting source files"
+  echo -e "source clean override applied, not deleting source files"
 fi
 
 if [[ $tmep_clean_override == "" ]]; then
   echo "no temp files clean override selected"
 elif [[ $temp_clean_override == "y" ]]; then
-  echo -e "temp clean override applied\keeping temp files for debugging"
+  echo -e "temp clean override applied, keeping temp files for debugging"
 fi
