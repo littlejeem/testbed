c435432a3aa903853f1d8732bf3b8d53373ca9ac
James Livingstone <james@Jamess-MacBook-Pro.local>
2020-09-04 21:42:15 +0100

    reordered position of elements

--- automatic_handbrake.sh
+++ automatic_handbrake.sh
@@ -7,11 +7,10 @@ echo "############################################################## - $logging_
 echo "############################################################## - $logging_date: Script Started - ##############################################################" > $log
 #
 #
-#+----------------------------------------------+
-#+---"Read In Command Line Overrides (flags)"---+
-#+----------------------------------------------+
-helpFunction()
-{
+#+-------------------+
+#+---Set functions---+
+#+-------------------+
+function helpFunction () {
    echo ""
    echo "Usage: $0 -r y -e y -t ## -q ## -s y -c y"
    echo "Usage: $0"
@@ -24,85 +23,7 @@ helpFunction()
    echo -e "\t-c Temp Override: By default the script removes any temp fileson completion. Setting parameter eg. -c y, will keep the files, useful if debugging"
    exit 1 # Exit script after printing help
 }
-while getopts r:e:t:q:s:c: flag
-do
-    case "${flag}" in
-        r) rip_only=${OPTARG};;
-        e) encode_only=${OPTARG};;
-        t) title_override=${OPTARG};;
-        q) quality_override=${OPTARG};;
-        s) source_clean_override=${OPTARG};;
-        c) temp_clean_override=${OPTARG};;
-        h) helpFunction;;
-    esac
-done
 #
-#+---------------------------------------------+#
-#+---Test selected Geopts flags for validity---+#
-#+---------------------------------------------+#
-# -r
-if [[ $rip_only == "" ]]; then
-  echo "no rip override, script will rip disc"
-#now test to make sure a number, see @Inian answer here https://stackoverflow.com/questions/41858997/check-if-parameter-is-value-x-or-value-y
-elif [[ "$rip_only" =~ ^(y|yes|Yes|YES|Y)$ ]]; then
-  echo "rip override selected, skipping rip"
-  else
-    echo "Error: -r is not a 'y' or 'yes'."
-    exit 2
-fi
-# -e
-if [[ $encode_only == "" ]]; then
-  echo "no encode override, script will encode to container"
-#now test to make sure a number, see @Inian answer here https://stackoverflow.com/questions/41858997/check-if-parameter-is-value-x-or-value-y
-elif [[ "$encode_only" =~ ^(y|yes|Yes|YES|Y)$ ]]; then
-  echo "encode override selected, skipping encode"
-  else
-    echo "Error: -e is not a 'y' or 'yes'"
-    exit 2
-fi
-# -t
-if [[ $title_override == "" ]]; then
-  echo "no title override applied"
-#now test to make sure a number, see @Joseph Shih answer here https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
-elif echo "$title_override" | grep -qE '^[0-9]+$'; then
-  echo -e "title override selected, chosen title is $title_override"
-  else
-    echo "Error: -t is not a number."
-    exit 2
-fi
-# -q
-if [[ $quality_override == "" ]]; then
-  echo "no quality override applied"
-#now test to make sure a number, see @Joseph Shih answer here https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
-elif echo "$quality_override" | grep -qE '^[0-9]+$'; then
-  echo -e "quality override selected, chosen title is $quality_override"
-else
-  echo "Error: -q is not a number."
-  exit 2
-fi
-# -s
-if [[ $source_clean_override == "" ]]; then
-  echo "no source clean override selected"
-elif [[ $source_clean_override == "y" ]]; then
-  echo -e "source clean override applied, not deleting source files"
-fi
-# -c
-if [[ $temp_clean_override == "" ]]; then
-  echo "no temp files clean override selected"
-elif [[ $temp_clean_override == "y" ]]; then
-  echo -e "temp clean override applied, keeping temp files for debugging"
-fi
-#
-#
-#+---------------------+
-#+---Source OMDB api---+
-#+---------------------+
-source /home/jlivin25/bin/omdb_key
-#
-#
-#+-------------------+
-#+---Set functions---+
-#+-------------------+
 function convert_secs () {
   #from here https://stackoverflow.com/questions/12199631/convert-seconds-to-hours-minutes-seconds
   num=$(echo $secs)
@@ -167,6 +88,87 @@ function prep_title_file() {
 }
 #
 #
+#+----------------------------------------------+
+#+---"Read In Command Line Overrides (flags)"---+
+#+----------------------------------------------+
+while getopts r:e:t:q:s:c: flag
+do
+    case "${flag}" in
+        r) rip_only=${OPTARG};;
+        e) encode_only=${OPTARG};;
+        t) title_override=${OPTARG};;
+        q) quality_override=${OPTARG};;
+        s) source_clean_override=${OPTARG};;
+        c) temp_clean_override=${OPTARG};;
+        h) helpFunction;;
+    esac
+done
+#
+#
+#+---------------------------------------------+#
+#+---Test selected Geopts flags for validity---+#
+#+---------------------------------------------+#
+# -r
+if [[ $rip_only == "" ]]; then
+  echo "no rip override, script will rip disc"
+#now test to make sure a number, see @Inian answer here https://stackoverflow.com/questions/41858997/check-if-parameter-is-value-x-or-value-y
+elif [[ "$rip_only" =~ ^(y|yes|Yes|YES|Y)$ ]]; then
+  echo "rip override selected, skipping rip"
+  else
+    echo "Error: -r is not a 'y' or 'yes'."
+    exit 2
+fi
+# -e
+if [[ $encode_only == "" ]]; then
+  echo "no encode override, script will encode to container"
+#now test to make sure a number, see @Inian answer here https://stackoverflow.com/questions/41858997/check-if-parameter-is-value-x-or-value-y
+elif [[ "$encode_only" =~ ^(y|yes|Yes|YES|Y)$ ]]; then
+  echo "encode override selected, skipping encode"
+  else
+    echo "Error: -e is not a 'y' or 'yes'"
+    exit 2
+fi
+# -t
+if [[ $title_override == "" ]]; then
+  echo "no title override applied"
+#now test to make sure a number, see @Joseph Shih answer here https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+elif echo "$title_override" | grep -qE '^[0-9]+$'; then
+  echo -e "title override selected, chosen title is $title_override"
+  else
+    echo "Error: -t is not a number."
+    exit 2
+fi
+# -q
+if [[ $quality_override == "" ]]; then
+  echo "no quality override applied"
+#now test to make sure a number, see @Joseph Shih answer here https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+elif echo "$quality_override" | grep -qE '^[0-9]+$'; then
+  echo -e "quality override selected, chosen title is $quality_override"
+else
+  echo "Error: -q is not a number."
+  exit 2
+fi
+# -s
+if [[ $source_clean_override == "" ]]; then
+  echo "no source clean override selected"
+elif [[ $source_clean_override == "y" ]]; then
+  echo -e "source clean override applied, not deleting source files"
+fi
+# -c
+if [[ $temp_clean_override == "" ]]; then
+  echo "no temp files clean override selected"
+elif [[ $temp_clean_override == "y" ]]; then
+  echo -e "temp clean override applied, keeping temp files for debugging"
+fi
+#
+#
+#+----------------------------+
+#+---Source necessary files---+
+#+----------------------------+
+source $HOME/.config/
+source /home/jlivin25/bin/omdb_key
+#
+#
 #+----------------------------+
 #+---Configure Disc Ripping---+
 #+----------------------------+
@@ -177,12 +179,18 @@ else
 fi
 echo $quality
 echo "quality selected is $quality" >> $log
-source_drive="disc:0"
-dev_drive="/dev/sr0"
-working_dir="/home/jlivin25"
-category="blurays"
-rip_dest="Rips"
-encode_dest="Encodes"
+###############################
+### Move to a settings file ###
+###############################
+  source_drive="disc:0"
+  dev_drive="/dev/sr0"
+  working_dir="/home/jlivin25"
+  category="blurays"
+  rip_dest="Rips"
+  encode_dest="Encodes"
+###############################
+###                         ###
+###############################
 bluray_name=$(blkid -o value -s LABEL "$dev_drive")
 bluray_name=${bluray_name// /_}
 echo "bluray name is $bluray_name" >> $log
