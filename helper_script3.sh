1196d29beef2a9c8d3c435243fb2fb20bb9c0459
James Livingstone <jlivin25@live.co.uk>
2020-11-29 21:39:18 +0000

    new script to try

--- helper_script3.sh
+++ helper_script3.sh
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+#
 tty -s && function log()     {     echo "$@"; }
 tty -s && function log_err() { >&2 echo "$@"; }
 tty -s || function log()     { logger -t $(basename $0) "$@"; }
 tty -s || function log_err() { logger -t $(basename $0) -p user.err "$@"; }
+
+
+
+# log()     { [[ -t 1 ]] &&     echo "$@" || logger -t $(basename $0) "$@"; }
+# log_err() { [[ -t 2 ]] && >&2 echo "$@" || logger -t $(basename $0) -p user.err "$@"; }
