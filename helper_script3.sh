00e8f656f5c93b8ef3299e7d718aa735116d8f40
James Livingstone <jlivin25@live.co.uk>
2020-11-30 18:13:53 +0000

    source referenced, example shown

--- helper_script3.sh
+++ helper_script3.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 #
+# from here
+# https://arnaudr.io/2015/12/06/bash-logging-helpers/
+#
+#
 tty -s && function log()     {     echo "$@"; }
 tty -s && function log_err() { >&2 echo "$@"; }
 tty -s || function log()     { logger -t $(basename $0) "$@"; }
 tty -s || function log_err() { logger -t $(basename $0) -p user.err "$@"; }
-
-
-
-# log()     { [[ -t 1 ]] &&     echo "$@" || logger -t $(basename $0) "$@"; }
-# log_err() { [[ -t 2 ]] && >&2 echo "$@" || logger -t $(basename $0) -p user.err "$@"; }
+#
+#example useage
+if ! command -v zip &> /dev/null
+then
+  log_err "ZIP could not be found, script won't function wihout it"
+  exit 1
+else
+  log "ZIP command located, continuing"
+fi
